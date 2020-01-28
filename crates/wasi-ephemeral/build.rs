14c0c999f62acf94c8736d071d760dba4b91ab7b
Daiki Ueno <dueno@redhat.com>
2020-01-28 18:30:38 +0100

    wasi-ephemeral: Read modularized witx files

--- crates/wasi-ephemeral/build.rs
+++ crates/wasi-ephemeral/build.rs
@@ -4,11 +4,24 @@ fn main() {
     let out_path: PathBuf = env::var_os("OUT_DIR").unwrap().into();
     let mut f = File::create(out_path.join("lib_generated.rs")).unwrap();
     const WASI_EPHEMERAL_WITX: &str =
-        "../generate-raw/WASI/phases/ephemeral/witx/wasi_ephemeral_preview.witx";
+        "../generate-raw/WASI/phases/ephemeral/witx";
     let witx_path: PathBuf = env::var_os("WASI_EPHEMERAL_WITX")
         .unwrap_or_else(|| WASI_EPHEMERAL_WITX.into())
         .into();
-    let out = generate_raw::generate(&[&witx_path]);
+    if !witx_path.is_dir() {
+        panic!("{:?} is not a directory", witx_path);
+    }
+    let mut paths = Vec::new();
+    for entry in witx_path.read_dir().expect("failed to read directory") {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+            let name = path.file_name().unwrap().to_str().unwrap();
+            if name.ends_with(".witx") {
+                paths.push(path);
+            }
+        }
+    }
+    let out = generate_raw::generate(&paths);
     write!(f, "{}", out).unwrap();
     println!("cargo:rerun-if-env-changed=WASI_EPHEMERAL_WITX");
     println!("cargo:rerun-if-changed={}", witx_path.display());
